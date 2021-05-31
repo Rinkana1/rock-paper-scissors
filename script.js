1de1f5edebedc6c7d74de712f790452ec95e74e0
Rinkana1 <mcjuno83@gmail.com>
2021-05-31 18:45:39 -0400

    Add Comments

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// Keeps count of the number of rounds the player has won
 var winCount = 0;
 
+// Randomly chooses between ROCK, PAPER, and SCISSORS for the computer
 function computerPlay() {
     var select = ['ROCK', 'PAPER', 'SCISSORS']
     var random = Math.floor(Math.random() * (3))
     return select[random]
 }
 
+// Plays a single round of Rock Paper Scissors
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase()
 
@@ -53,12 +56,14 @@ function playRound(playerSelection, computerSelection) {
             }
             break;
         default:
+            // If the player gives an invalid response, restart the round
             console.log('This isn\'t a valid answer. Please try again');
             return playRound(prompt(), computerPlay())
             
     }
 }
 
+// Loops 5 rounds and logs the results to the console. Then Declares who wins depending on the score.
 function game() {
     for (var i = 0; i <= 4; i++) {
         console.log(playRound(prompt(), computerPlay()));
